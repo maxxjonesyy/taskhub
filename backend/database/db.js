66843b3201807f4b91edcfec2af6d06fcb31c6cf
Max Jones <maxxjonesyy@gmail.com>
2024-04-04 21:52:34 +1100

    setup mongodb connection

---
+++ backend/database/db.js
@@ -0,0 +1,24 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const url = process.env.MONGO;
+
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB.");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+module.exports = { run };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const db = require("./database/db");
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+db.run()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`Error starting server: ${error}`);
+  });
