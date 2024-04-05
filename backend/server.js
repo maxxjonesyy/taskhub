c36934437ac41cb7f7bb128d0d3434c653a37794
Max Jones <maxxjonesyy@gmail.com>
2024-04-05 18:56:52 +1100

    change backend port no.

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./database/db");
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
-db.run()
-  .then(() => {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  })
-  .catch((error) => {
-    console.log(`Error starting server: ${error}`);
-  });
+db.run().then(() => {
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+});
