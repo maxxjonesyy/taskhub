fdd0d39fe6e66bdd1360a4ef2566a608bd952c0c
Max Jones <maxxjonesyy@gmail.com>
2024-04-06 12:56:43 +1100

    setup react router and login template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./views";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path='/login' element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/views/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h1>Taskhub</h1>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
