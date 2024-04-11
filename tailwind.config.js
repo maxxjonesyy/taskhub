ffc6390e6cc13599f842b166894af5f87bc913ee
Max Jones <maxxjonesyy@gmail.com>
2024-04-11 20:38:16 +1000

    import poppins font & base theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "accent-red": "var(--accent-red)",
+        "accent-grey": "var(--accent-grey)",
+        "text-grey": "var(--text-grey)",
+        "background-light": "var(--background-light)",
+        "background-dark": "var(--background-dark)",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
