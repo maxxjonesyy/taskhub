a170abf2995d887554f733084b2dad7a99ac30e4
Max Jones <maxxjonesyy@gmail.com>
2024-04-13 17:55:28 +1000

    add bodyParser to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const db = require("./database/db");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json({ limit: "1kb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "1kb" }));
+
 app.use(cors());
+
+app.use("/", require("./routes/login"));
 app.use("/", require("./routes/register"));
 
-db.once("open", () => {
+db.once("open", (error) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 });
-
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
