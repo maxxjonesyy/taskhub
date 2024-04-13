edeb50a3b15fa58764550bb1446b6508f71f5ea0
Max Jones <maxxjonesyy@gmail.com>
2024-04-13 17:55:56 +1000

    register component and backend validation

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -2,38 +2,48 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const validator = require("validator");
 
 router.post("/api/register", async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const email = validator.escape(req.body.email);
+    const password = validator.escape(req.body.password);
 
     if (!email || !password) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
-    if (password.length < 8) {
+    if (!validator.isEmail(email)) {
       return res
         .status(400)
-        .json({ error: "Password must be at least 8 characters long" });
+        .json({ error: "Please enter a correct email address" });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({
+        error: `Password must contain: 
+        At least 8 characters
+        At least 1 uppercase letter
+        At least 1 lowercase letter
+        At least 1 number`,
+      });
     }
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      return res.status(400).json({ error: "Email already exists" });
+      return res.status(400).json({ error: "Email is already in use" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
       email,
-      password: hashedPassword,
+      password: await bcrypt.hash(password, 10),
     });
 
     await newUser.save();
 
     res.status(200).json({
-      message: "Your Taskhub account has been created!",
+      message: "Registration successful",
     });
   } catch (error) {
     console.error("Error registering user:", error);
---
+++ src/components/Register.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { sendFormData } from "../utils";
+
+function Register({ setIsLogin }: { setIsLogin: Function }) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div className='w-full'>
+      <form
+        onSubmit={(event) =>
+          sendFormData({
+            event,
+            endpoint: "/api/register",
+            email,
+            password,
+            setEmail,
+            setPassword,
+          })
+        }
+        className='w-[350px] flex flex-col justify-center mx-auto gap-6'>
+        <div>
+          <h2 className='text-3xl font-bold mb-2'>Register</h2>
+          <p className='text-accent-grey'>
+            Enter your details below to create an account
+          </p>
+        </div>
+
+        <input
+          className='bg-transparent border border-accent-grey rounded-md p-3 placeholder:accent-grey'
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+
+        <input
+          className='bg-transparent border border-accent-grey rounded-md p-3 placeholder:accent-grey'
+          type='password'
+          placeholder='Password'
+          minLength={8}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+
+        <button className='bg-accent-red font-medium rounded-md p-2.5 shadow-lg transition-transform hover:scale-105'>
+          Register
+        </button>
+
+        <button
+          className='underline'
+          onClick={(event) => {
+            event.preventDefault();
+            setIsLogin(true);
+          }}>
+          Already have an account?
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
