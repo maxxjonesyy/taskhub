f1d0dab4e63615f44531c31b105315a32407e6d5
Max Jones <maxxjonesyy@gmail.com>
2024-04-22 21:39:26 +1000

    change flex to grid

--- src/views/Auth.tsx
+++ src/views/Auth.tsx
@@ -5,10 +5,10 @@ function Auth() {
   const [isLogin, setIsLogin] = useState<boolean>(true);
 
   return (
-    <div className='min-h-screen max-w-[600px] mx-auto grid py-5 bg-gradient-to-b from-background-primary via-background-secondary to-background-primary'>
+    <div className='min-h-screen max-w-[600px] mx-auto flex flex-col justify-center py-5 bg-gradient-to-b from-background-primary via-background-secondary to-background-primary'>
       <img
-        className='mx-auto mb-16'
-        width={350}
+        className='mx-auto mb-20'
+        width={300}
         src='src/assets/logo.svg'
         alt='Taskhub logo'
       />
