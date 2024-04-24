0fb1019b7ced921169b7047433e383ff06260197
Maxwell Jones <maxxjonesyy@gmail.com>
2024-04-24 16:21:40 +1000

    create dashboard structure

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,5 +1,15 @@
+import { Navbar } from "../components";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <section className="flex flex-col lg:flex-row">
+      <Navbar />
+
+      <div className="flex-1 p-5">
+        <p>Dashboard goes here</p>
+      </div>
+    </section>
+  );
 }
 
 export default Dashboard;
