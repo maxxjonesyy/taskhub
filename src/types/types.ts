3f4c4db245bfb12d0bcd33b3f2f108b2a8d27b5b
Max Jones <maxxjonesyy@gmail.com>
2024-04-29 21:00:54 +1000

    create project and user types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,18 @@
+import { Key } from "react";
 
+export type User = {
+  id: Key;
+  email: String;
+  token: String;
+  username: String;
+};
+
+export type Project = {
+  createdAt: Date;
+  createdBy: String;
+  name: String;
+  _id: Key;
+  tasks: Array<String>;
+  updatedAt: Date;
+  __v: Number;
+};
