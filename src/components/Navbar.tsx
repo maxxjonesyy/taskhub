0931fb7323ca2e77675cd844692b09e2cd5e6586
Max Jones <maxxjonesyy@gmail.com>
2024-05-13 21:14:59 +1000

    refactor: fix onclick element for creating project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -91,14 +91,14 @@ function Navbar({ user, projects, setProjects }: NavbarProps) {
       )}
 
       {menu === "projects" && (
-        <aside
-          onClick={() => {
-            const createProject = document.getElementById("create-project");
-            createProject?.classList.remove("hidden");
-            createProject?.classList.add("inline-flex");
-          }}
-          className='absolute mt-5 p-5 w-[325px] rounded-md bg-background-secondary shadow-xl'>
-          <button className='w-full inline-flex items-center justify-center gap-2 bg-transparent border border-secondary rounded-md p-2 font-medium shadow-lg transition-transform hover:scale-105'>
+        <aside className='absolute mt-5 p-5 w-[325px] rounded-md bg-background-secondary shadow-xl'>
+          <button
+            onClick={() => {
+              const createProject = document.getElementById("create-project");
+              createProject?.classList.remove("hidden");
+              createProject?.classList.add("inline-flex");
+            }}
+            className='w-full inline-flex items-center justify-center gap-2 bg-transparent border border-secondary rounded-md p-2 font-medium shadow-lg transition-transform hover:scale-105'>
             <img src='./src/assets/icons/plus-icon.svg' alt='create project' />
             <span>Create Project</span>
           </button>
