4886cbb757d5b8547fa504fc843c7279b931f931
Max Jones <maxxjonesyy@gmail.com>
2024-05-16 21:27:50 +1000

    update render alert to match theme

--- src/utils/renderAlert.tsx
+++ src/utils/renderAlert.tsx
@@ -7,6 +7,8 @@ function renderAlert(icon: SweetAlertIcon, message: string) {
     showConfirmButton: false,
     timer: 5000,
     timerProgressBar: true,
+    background: "#292929",
+    color: "#d8d8d8",
     didOpen: (toast) => {
       toast.onmouseenter = Swal.stopTimer;
       toast.onmouseleave = Swal.resumeTimer;
