a6d6839b1a43362074e718b0f29ab03ec380486d
Max Jones <maxxjonesyy@gmail.com>
2024-05-17 13:26:51 +1000

    setup loading and conditional renders for dashboard

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
-import { Navbar, WelcomeScreen } from "../components";
+import { Navbar, WelcomeScreen, ActiveProject } from "../components";
+import { ActiveProjectType } from "../types/types";
+import { PulseLoader } from "react-spinners";
 import axios from "axios";
 
 function Dashboard() {
   const user = JSON.parse(localStorage.getItem("user") || "{}");
   const [projects, setProjects] = useState([]);
+  const [activeProject, setActiveProject] = useState<ActiveProjectType>();
+  const [loading, setLoading] = useState<boolean>(true);
 
   async function getProjects() {
+    setLoading(true);
+
     try {
       const response = await axios.get(`api/get-projects/${user.id}`, {
         headers: {
@@ -16,27 +22,40 @@ function Dashboard() {
 
       if (response.status === 200) {
         setProjects(response.data.projects);
+        setActiveProject(response.data.projects[0]);
       }
     } catch (error: any) {
       console.error(error.response.data.error);
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
     getProjects();
-  }, [projects]);
+  }, []);
 
   return (
     <section className='min-h-screen flex flex-col'>
       <Navbar user={user} projects={projects} setProjects={setProjects} />
 
-      <div className='flex-1 p-5'>
-        <WelcomeScreen
-          user={user}
-          projects={projects}
-          setProjects={setProjects}
-        />
-      </div>
+      {loading ? (
+        <div className='flex-1 flex items-center justify-center'>
+          <PulseLoader color='#FFFFFF' size={12} />
+        </div>
+      ) : (
+        <div className='flex-1 p-5'>
+          {projects.length > 0 ? (
+            <ActiveProject project={activeProject} />
+          ) : (
+            <WelcomeScreen
+              user={user}
+              projects={projects}
+              setProjects={setProjects}
+            />
+          )}
+        </div>
+      )}
     </section>
   );
 }
