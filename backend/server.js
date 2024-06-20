05eb3a1a9d1259e6ae9eed4bfaf40ff33a9f9c45
Maxwell Jones <maxxjonesyy@gmail.com>
2024-06-20 13:37:02 +1000

    configure allowed origins

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,22 @@ const { escapeInputs, verifyAccessToken } = require("./middleware/middleware");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173", // Localhost 1
+  "http://localhost:5174", // Localhost 2
+  "https://taskhub-mj.netlify.app", // Production
+];
+
 const corsOptions = {
-  origin: "https://taskhub-mj.netlify.app",
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   optionsSuccessStatus: 200,
-}
+};
 
 app.use(express.json({ limit: "1kb" }));
 app.use(express.urlencoded({ extended: true, limit: "1kb" }));
