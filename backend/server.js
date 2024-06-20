d9a3e2118589c9afdd99036531998dbd0c4530cc
Maxwell Jones <maxxjonesyy@gmail.com>
2024-06-20 11:41:37 +1000

    add cors options for netlify site

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ const { escapeInputs, verifyAccessToken } = require("./middleware/middleware");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+const corsOptions = {
+  origin: "https://taskhub-mj.netlify.app",
+  optionsSuccessStatus: 200,
+}
+
 app.use(express.json({ limit: "1kb" }));
 app.use(express.urlencoded({ extended: true, limit: "1kb" }));
 app.use(escapeInputs);
-app.use(cors());
-
-app.use(escapeInputs);
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/", require("./routes/authRoutes"));
 app.use("/", verifyAccessToken, require("./routes/databaseRoutes"));
